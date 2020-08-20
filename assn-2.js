9389e2df1da96e0e47dd75d44e8039dd4c00d7eb
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-20 18:53:17 -0500

    completes question 4

--- assn-2.js
+++ assn-2.js
@@ -95,4 +95,21 @@ function findLast(linkedList) {
   return currNode;
 }
 
+// O(n^2)
+function WhatDoesThisProgramDo(lst) {
+  let current = lst.head;
+  while (current !== null) {
+    let newNode = current;
+    while (newNode.next !== null) {
+      if (newNode.next.value === current.value) {
+        newNode.next = newNode.next.next;
+      }
+      else {
+        newNode = newNode.next;
+      }
+    }
+    current = current.next;
+  }
+}
+
 main();
